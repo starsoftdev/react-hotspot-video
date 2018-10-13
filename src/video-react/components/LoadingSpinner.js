4c33ae90ed3d72d72b1fb4147cb4385d5aca469b
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-10-12 21:36:21 -0500

    Initial Commit

---
+++ src/video-react/components/LoadingSpinner.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+import { play } from '../actions/player';
+
+const propTypes = {
+  player: PropTypes.object,
+  className: PropTypes.string,
+};
+
+export default function LoadingSpinner({ player, className }) {
+  if (player.error) {
+    return null;
+  }
+  return (
+    <div
+      className={classNames(
+        'video-react-loading-spinner',
+        className
+      )}
+    />
+  );
+}
+
+LoadingSpinner.propTypes = propTypes;
+LoadingSpinner.displayName = 'LoadingSpinner';
